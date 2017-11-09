bfe9d77b0a80a8f05bdf3f836795e5c051180808
nardtree <gim.kobayashi@gmail.com>
2017-11-10 01:27:58 +0900

    a

--- mona_coin.py
+++ mona_coin.py
@@ -8,7 +8,7 @@ import keras
 
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential, Model, load_model
-from keras.layers import Input, Activation, Dropout, Flatten, Dense, Reshape, merge
+from keras.layers import Lambda, Input, Activation, Dropout, Flatten, Dense, Reshape, merge
 from keras.preprocessing.image import ImageDataGenerator
 from keras import optimizers
 from keras.preprocessing.image import ImageDataGenerator
@@ -18,7 +18,7 @@ from keras.applications.vgg19 import VGG19
 from keras.applications.vgg16 import VGG16
 from keras.applications.resnet50 import ResNet50
 from keras.optimizers import SGD, Adam
-
+from keras import backend as K
 
 input_tensor = Input( shape=(10, 35) )
 
@@ -29,11 +29,22 @@ x = Dropout(0.3)(x)
 x = Dense(3000, activation='relu')(x)
 x = Dropout(0.3)(x)
 x = Flatten()(x)
-x = Dense(3000, activation='relu')(x)
-x = Dense(1, activation='linear')(x)
+x1 = Dense(1000, activation='relu')(x)
+x2 = Dense(1000, activation='relu')(x)
 
-model = Model(inputs=input_tensor, outputs=x)
-model.compile(loss='mae', optimizer='sgd')
+
+h = Dense(1, activation='linear')(x1)
+m = Dense(1, activation='linear')(x2)
+
+epsilon_std = 1.0
+def sampling(args):
+  z_mean, z_log_var = args
+  epsilon = K.random_normal(shape=(K.shape(z_mean)[0], 1), mean=0., stddev=epsilon_std)
+  return z_mean + K.exp(z_log_var / 2) * epsilon
+z = Lambda(sampling, output_shape=(1,))([m, h])
+
+model = Model(inputs=input_tensor, outputs=z)
+model.compile(loss='mae', optimizer='adam')
 
 ys, Xs = pickle.loads( gzip.decompress( open('tmp/data.pkl', 'rb').read() ) )
 max_ = ys.max()
